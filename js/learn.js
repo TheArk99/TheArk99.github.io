eec959551bf26b83048170429e5fc1f9b286c07b
Noah McMillan <noahfrmc@gmail.com>
2021-07-21 03:27:31 -0700

    upgrade

---
+++ js/learn.js
@@ -0,0 +1,44 @@
+// var number, number2, number3;
+// number = 30;
+// number2 =  45;
+// number3 = 4598;
+// number4 = document.getElementById('num').value + 10;
+
+// var total = number + number2 + number3;
+
+// var totalStr = total.stringify;
+
+// document.getElementById('num').innerHTML = total;
+
+// // document.getElementById('num').innerHTML = 'hello world';
+
+
+
+
+// var something = question;
+
+
+
+
+
+
+
+// for (var i = 0; i = 1; i++) {
+// }
+
+// var i = 0;
+// while (i < 100) { 
+//     i++;
+// }
+
+// console.log(i);
+
+
+
+
+
+
+
+
+
+document.getElementById("somethintg").innerHTML = i;
